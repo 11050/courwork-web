6191459910c453e4467692e3e7edf6c7df4a707f
11050 <wstudent898@gmail.com>
2021-12-09 13:59:43 +0300

    click button

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+function myFunction(x) {
+    x.classList.toggle("fa-thumbs-down");
+  }
 
+  function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
